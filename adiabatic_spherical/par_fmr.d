71ba0f88c4849a13882da8e227ac21269ffe843b
cmyoo <chulmoon.yoo@gmail.com>
2024-12-16 18:17:17 +0900

    [Add] adiabatic spherical sample folder added

---
+++ adiabatic_spherical/par_fmr.d
@@ -0,0 +1,22 @@
+############################################################
+##  parameters for FMR in COSMOS code    ###################
+##  ver1.00  by Chulmoon Yoo on          ###################
+############################################################
+##  maximum number of fmr layers 
+2		
+##x-grid number for fmr region
+10
+20
+0
+##y-grid number for fmr region
+10
+20
+0
+##z-grid number for fmr region
+10
+20
+0
+##values of the lapse for starting fmr
+0.15
+0.1
+0.
