8390bcf33c47aafc4510f786ee4ac000ccb32862
cmyoo <chulmoon.yoo@gmail.com>
2025-01-21 20:14:50 +0900

    [Modify] par_ini.d and par_fmr.d in adiabatic_spherical modified

--- source/adiabatic_spherical/par_fmr.d
+++ source/adiabatic_spherical/par_fmr.d
@@ -5,16 +5,16 @@
 ##  maximum number of fmr layers 
 2		
 ##x-grid number for fmr region
-10
-10
+9
+9
 0
 ##y-grid number for fmr region
-10
-10
+9
+9
 0
 ##z-grid number for fmr region
-10
-10
+9
+9
 0
 ##values of the lapse for starting fmr
 0.3
